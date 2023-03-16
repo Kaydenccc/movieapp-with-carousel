ebe8d999a9d979812dc7ff41901e1022db440bfe
Kaydenccc <ffadilah682@gmail.com>
2023-03-16 14:39:51 +0800

    fiex the components buttonplus

--- src/components/ButtonPlus.tsx
+++ src/components/ButtonPlus.tsx
@@ -3,7 +3,7 @@ import { AiOutlinePlus } from 'react-icons/ai';
 
 const ButtonPlus = () => {
   return (
-    <button className="p-[10px] h-[44px]  text-xl h-full bg-gray-500/70 backdrop-blur-sm">
+    <button className="p-[10px] h-[44px]  text-xl bg-gray-500/70 backdrop-blur-sm">
       <AiOutlinePlus className="font-bold" />
     </button>
   );
